a86b47eaedcc665c0e5b7abb839f5c817de81dbb
GunPKate <gunPK>
2024-01-25 22:16:47 +0700

    search input

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,FlatList
+ } from 'react-native'
 
 import styles from './welcome.style'
+import { useRouter } from 'expo-router' 
+import { icons,SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View>
+        <View style={styles.container}>
+          <Text style={styles.userName}>Hello Adrian</Text>
+          <Text style={styles.WelcomeMessage}>Find your perfect job</Text>
+        </View>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={ styles.searchInput}
+            value = ""            
+            onChange = {()=>{}}
+          />
+        </View>
+      </View>
     </View>
   )
 }
