46ba3a9c8f1410420be709d7f877c56918339b74
GunPKate <gunPK>
2024-01-28 19:44:50 +0700

    get data from nearby job

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,6 +1,15 @@
-import {Text} from 'react-native'
+import {Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native'
+import useFetch from '../../hook/useFetch';
+// import { Company, JobAbout,JobFooter,JobTabs, ScreenHeaderBtn } from '../../components/jobdetails'
+import { COLORS, icons, SIZES } from '../../constants'
+import { useRouter, Stack, useLocalSearchParams} from 'expo-router';
 
 const jobDetails = () =>{
+    const params = useLocalSearchParams();
+    const router = useRouter();
+
+    const {data, isLoading, error, refetch} = useFetch('job-details',{job_id: params.id})
+    console.log(data)
     return (
         <Text>1234</Text>
     )
